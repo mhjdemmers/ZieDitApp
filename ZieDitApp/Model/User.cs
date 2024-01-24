3722f598719721291fd5dc4511c957f9a8cc8583
LasseAupperle <39459681+LasseAupperle@users.noreply.github.com>
2024-01-24 13:04:44 +0100

    Intstalled Nuget packages and added user class

---
+++ ZieDitApp/Model/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace ZieDitApp.Model
+{
+    class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [SQLite.Column("name"), Indexed, SQLite.NotNull]
+        public string? Name { get; set; }
+
+        [Unique]
+        public string? Password { get; set; }
+    }
+}
