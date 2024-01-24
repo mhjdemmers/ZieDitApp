6f25df492f502892476c3b7fdafa79af00dbedf9
LasseAupperle <39459681+LasseAupperle@users.noreply.github.com>
2024-01-24 13:46:08 +0100

    Organizer aangemaakt

---
+++ ZieDitApp/Model/Organizer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLitePCL;
+
+namespace ZieDitApp.Model
+{
+    class Organizer : User
+    {
+        public List<Event>? OwnedEvents { get; set; }
+    }
+}
