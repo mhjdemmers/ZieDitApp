143805113de71a3e3a5f19b1833eff70f02acf8f
LasseAupperle <39459681+LasseAupperle@users.noreply.github.com>
2024-01-29 11:44:43 +0100

    Constants aangemaakt (Database connection)

---
+++ ZieDitApp/Constants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace ZieDitApp
+{
+    internal class Constants
+    {
+        private const string dBFileName = "ZieDitDB";
+
+        public const SQLiteOpenFlags flags =
+            SQLiteOpenFlags.ReadWrite |
+            SQLiteOpenFlags.Create |
+            SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath
+        {
+            get
+            {
+                return Path.Combine(FileSystem.AppDataDirectory, dBFileName);
+            }
+        }
+    }
+}
