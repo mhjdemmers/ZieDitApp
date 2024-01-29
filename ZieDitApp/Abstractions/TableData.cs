0c9a9974efbf628307918a28dd7c992f5d00db05
LasseAupperle <39459681+LasseAupperle@users.noreply.github.com>
2024-01-29 11:43:18 +0100

    Abstractions IbaseRepository en TableData toegevoegd

---
+++ ZieDitApp/Abstractions/TableData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace ZieDitApp.Abstractions
+{
+    public class TableData
+    {
+        [PrimaryKey, AutoIncrement] public int Id { get; set; }
+    }
+}
