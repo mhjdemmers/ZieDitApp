499eb21fc920f14600108975d36e2b27c450cc0d
LasseAupperle <39459681+LasseAupperle@users.noreply.github.com>
2024-01-30 16:21:38 +0100

    Added login Register part missing

--- ZieDitApp/ViewModel/BaseViewModel.cs
+++ ZieDitApp/ViewModel/BaseViewModel.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace ZieDitApp.ViewModel
 {
-    class BaseViewModel : INotifyPropertyChanged
+    public class BaseViewModel : INotifyPropertyChanged
     {
         bool isBusy = false;
         string title;
@@ -37,9 +37,84 @@ namespace ZieDitApp.ViewModel
 
         public bool IsNotBusy => !IsBusy;
 
-        public event PropertyChangedEventHandler? PropertyChanged;
+        //public event PropertyChangedEventHandler? PropertyChanged;
 
-        public void OnPropertyChanged([CallerMemberName] string name = null) =>
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        //public void OnPropertyChanged([CallerMemberName] string name = null) =>
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        public BaseViewModel()
+        {
+
+        }
+
+        public async Task<String> LoadPhotoAsync(FileResult photo)
+        {
+            var stream = photo.OpenReadAsync().Result;
+
+            byte[] imagedata;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                stream.CopyTo(ms);
+                imagedata = ms.ToArray();
+            }
+
+            var folderpath = Path.Combine(FileSystem.AppDataDirectory, "EmployeePhoto");
+            if (!File.Exists(folderpath))
+            {
+                Directory.CreateDirectory(folderpath);
+            }
+
+            var empfilename = Guid.NewGuid() + "_employeephoto.jpg";
+
+            var newfile = Path.Combine(folderpath, empfilename);// Complete Path of the photo
+
+            using (var stream2 = new MemoryStream(imagedata))
+            using (var newstream = File.OpenWrite(newfile))
+            {
+                await stream2.CopyToAsync(newstream);
+            }
+
+            return newfile;
+        }
+        public virtual void OnAppearing()
+        {
+
+        }
+        public virtual void OnDisappearing()
+        {
+
+        }
+
+        protected bool SetProperty<T>(ref T backingstore, T value,
+            [CallerMemberName] string propertyName = "", Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingstore, value))
+                return false;
+
+            backingstore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changes = PropertyChanged;
+            if (changes == null)
+            {
+                return;
+            }
+
+            changes.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
     }
+
+
 }
---
+++ ZieDitApp/ViewModel/LoginViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ZieDitApp.ViewModel
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public LoginViewModel()
+        {
+
+        }
+
+        public bool isRunning;
+        public bool IsRunning
+        {
+            get { return isRunning; }
+            set { SetProperty(ref isRunning, value); }
+
+        }
+
+        public string userName;
+        public string UserName
+        {
+            get { return userName; }
+            set { SetProperty(ref userName, value); }
+
+        }
+
+        public string password;
+        public string Password
+        {
+            get { return password; }
+            set { SetProperty(ref password, value); }
+
+        }
+
+        private ICommand _loginCommand;
+        public ICommand LoginCommand =>
+             _loginCommand ?? (_loginCommand = new Command(async () => await ExecuteLoginAsync()));
+
+
+        public async Task ExecuteLoginAsync()
+        {
+            if (IsRunning)
+                return;
+            IsRunning = true;
+
+
+            if (string.IsNullOrEmpty(UserName))
+            {
+                await Shell.Current.DisplayAlert("Missing UserName", "Please provide valid UserName", "OK");
+                IsRunning = false;
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Password))
+            {
+                await Shell.Current.DisplayAlert("Missing Password", "Please provide valid Password", "OK");
+                IsRunning = false;
+                return;
+            }
+        }
+
+    }
+}
