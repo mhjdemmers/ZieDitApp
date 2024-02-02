641445e27c0c67a688df0c81f993d47379fb2281
LasseAupperle <39459681+LasseAupperle@users.noreply.github.com>
2024-02-02 13:08:17 +0100

    primary key issue resolved

---
+++ ZieDitApp/Repositories/EventUserRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZieDitApp.Model;
+
+namespace ZieDitApp.Repositories
+{
+   
+        public class EventUserRepository : BaseRepository<EventUser>
+        {
+            // Other methods...
+
+            public void AddEventUser(EventUser eventUser)
+            {
+                try
+                {
+                    SaveEntity(eventUser);
+                }
+                catch (Exception ex)
+                {
+                    StatusMessage = $"Error: {ex.Message}";
+                }
+            }
+
+
+        }
+}
